b4070efe86c84364ba3d234bc9be14cfb4e0514b
theworldbright <kent@kentshikama.com>
2015-11-01 16:15:35 -0800

    Minor refactorings

--- main.m
+++ main.m
@@ -3,35 +3,47 @@ close all
 
 %% Configure variables here
 center_image_name = 'middle';
-input_dir = 'aoi/';
-output_dir = './out/'; % Make sure you have mkdir'ed this directory
-input_file_ext = 'jpg';
+input_dir = 'out/';
+output_dir = './new_out/'; % Make sure you have mkdir'ed this directory
+input_file_ext = 'tif';
 output_file_ext = 'tif';
 canvas_scale_of_center_image = 5; % Canvas size will be this times size of center image
 
 %% Don't touch below here
-center = rgb2gray(im2double(imread([input_dir center_image_name '.' input_file_ext])));
-canvas_size = length(center) * canvas_scale_of_center_image;
-files = dir([input_dir '*.' input_file_ext]);
-peripheral_images = [];
+% center = rgb2gray(im2double(imread([input_dir center_image_name '.' input_file_ext])));
+% canvas_size = length(center) * canvas_scale_of_center_image;
+% files = dir([input_dir '*.' input_file_ext]);
+% peripheral_images = [];
+% count = 1;
+% for file = files'
+%     file_name = file.name;
+%     if strcmp(file_name, [center_image_name '.' input_file_ext])
+%         % Skip center image
+%     else
+%         I = imread([input_dir file_name]);
+%         I = rgb2gray(im2double(I));
+%         I = warpImage(center, I, canvas_size);        
+%         imwrite(I, [output_dir file_name(1:end-3) output_file_ext]);
+%         peripheral_images{count} = I;
+%         count = count + 1;
+%     end
+% end
+% center = offsetImage(center, canvas_size, 0, 0);
+% imwrite(center, [output_dir 'middle' '.' output_file_ext]);
+
+%% Test code
+center = im2double(imread([input_dir center_image_name '.' input_file_ext]));
+canvas_size = length(center);
 count = 1;
+peripheral_images = [];
+files = dir([input_dir '*.' input_file_ext]);
 for file = files'
     file_name = file.name;
-    if strcmp(file_name, [center_image_name '.' input_file_ext])
-        % Skip center image
-    else
-        I = imread([input_dir file_name]);
-        I = rgb2gray(im2double(I));
-        I = warpImage(center, I, canvas_size);
-        
-        I(isnan(I)) = 0;
-        imwrite(I, [output_dir file_name(1:end-3) output_file_ext]);
-        peripheral_images{count} = I;
-        count = count + 1;
-    end
+    I = imread([input_dir file_name]);
+    I = im2double(I);
+    peripheral_images{count} = I;
+    count = count + 1;
 end
-center = offsetImage(center, canvas_size, 0, 0);
-imwrite(center, [output_dir 'middle' '.' output_file_ext]);
 
 I = blendedAssembly(center, peripheral_images, canvas_size);
 imwrite(I, [output_dir 'mosaic' '.' output_file_ext]);
--- offsetImage.m
+++ offsetImage.m
@@ -1,7 +1,9 @@
 function offsetted_image = offsetImage(image, canvas_size, additional_offset_x, additional_offset_y)
     [h,w] = size(image);
     offsetted_image = zeros(canvas_size, canvas_size);
-    offset = canvas_size/3;
+    offset = round(canvas_size/3);
+    additional_offset_y = round(additional_offset_y);
+    additional_offset_x = round(additional_offset_x);
     for i = 1:1:h
        for j = 1:1:w
            offsetted_image(i+offset+additional_offset_y,j+offset+additional_offset_x) = image(i,j);
